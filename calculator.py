5cfb816535b7ef040a9bdc7e166eb6656569f6e6
michischmidt <schmidt.m.dev@gmail.com>
2021-04-27 23:53:46 +0200

    base for calculator implementation

--- calculator.py
+++ calculator.py
@@ -4,30 +4,51 @@ import sys
 
 
 class Calculator(QMainWindow):
+
     def __init__(self):
         super().__init__()
-        # self.count = 0
         self.initUI()
+        self.__result = 0
+        self.__input = ""
+        self._setKeyboardKeys()
+
+    def _setKeyboardKeys(self): 
+        self.__KEYBOARD_DIGIT = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.__KEYBOARD_OPERATOR = [".", ",", "/", "*", "-", "+", "="]
 
     def initUI(self):
         self.ui = uic.loadUi("calculator.ui", self)
         self.show()
         # self.ui.pushButton.clicked.connect(self.countUp)
 
+    def checkInput(self, key):
+        print(key)
+
     def countUp(self):
         self.count += 1
-        self.ui.lcdDisplay.display()
+        self.ui.lineDisplay.display()
 
-    # key input
+    # key inputs
     def keyPressEvent(self, event):
-        if event.text() == 'r':
-            self.count = 0
-            self.ui.lcdDisplay.display(self.count)
+        # if(self.checkInput(event.key())):
+        #     # calculate
+        #     print(0)
+        # else:
+        #     # ignore
+        #     print("1")
+
+        if event.text() == "1":
+            self.__input += "1"
+
+        if event.text() == "+":
+            self.__input += "+"
+
+        self.ui.labelResult.setText(self.__input)
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    cnt = Calculator()
+    calculator = Calculator()
     sys.exit(app.exec_())
 
 
